b40d2349c999e43bc5059e343ca4bae747f284e9
chiaqingwu <secret8419@gmail.com>
2019-05-27 00:03:11 +0800

    [fix] Change secret, token and id and set ignore

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const linebot = require('linebot');
 const translate = require('@k3rn31p4nic/google-translate-api');
 const bot = linebot({
-    channelId: '1577605735',
-    channelSecret: '62974195cfc83efca8dd6fcc4f0b59aa',
-    channelAccessToken: '7bpwTEYLp0/EC7WEr08+M4Lih0GNIurFoQuSTANVv2hU3v+J+6VI/X+tXtiNkOkozTQa2FajyCB8jm/p6jV9IAypJ4oSaLt8LVj+/Y4Jj2WSMiCygQDIAl1w0W+aYzGDswtZb11jZtaeKD89tlpSEwdB04t89/1O/w1cDnyilFU='
+    channelId: CHANNEL_ID,
+    channelSecret: CHANNEL_SECRET,
+    channelAccessToken: CHANNEL_ACCESS_TOKEN
 });
 
 let languages = [
